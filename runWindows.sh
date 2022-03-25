8c75afdb8874c280e6785a041a59e3498f0cf182
Daniel Yang Hansen <daniel.yang.hansen@gmail.com>
2022-03-25 16:38:32 +0100

    cleanup

--- runWindows.sh
+++
@@ -1,6 +0,0 @@
-
-echo "Setting up the database..."
-python .\KaffeDBTestInit.py
-echo "running tests..."
-python .\KaffeDBUserStoryTests.py | Out-File -FilePath .\Output
-echo "Results successfully stored into file: Output"
